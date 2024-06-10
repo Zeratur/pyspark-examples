322f751d70896cd0c8aa27ed0caa94a7b78023c3
pandacheng <vicxen.pan@gmail.com>
2024-06-10 15:12:30 +0800

    update format

--- pyspark-dataframe-repartition.py
+++ pyspark-dataframe-repartition.py
@@ -5,13 +5,14 @@ author SparkByExamples.com
 
 import pyspark
 from pyspark.sql import SparkSession
-spark = SparkSession.builder.appName('SparkByExamples.com') \
-        .master("local[5]").getOrCreate()
 
-df=spark.range(0,20)
+spark = SparkSession.builder.appName('SparkByExamples.com') \
+    .master("local[5]").getOrCreate()
+
+df = spark.range(0, 20)
 print(df.rdd.getNumPartitions())
 
-df.write.mode("overwrite").csv("c:/tmp/partition.csv")
+# df.write.mode("overwrite").csv("c:/tmp/partition.csv")
 
 df2 = df.repartition(6)
 print(df2.rdd.getNumPartitions())
